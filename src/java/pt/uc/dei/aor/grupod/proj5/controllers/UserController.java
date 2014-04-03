040f2472e5ed49e4027b8ac99d30d9bf0cb597d2
bjscosta <bscosta8@sapo.pt>
2014-04-03 12:39:30 +0100

    o que não está na rede não existe

--- src/java/pt/uc/dei/aor/grupod/proj5/controllers/UserController.java
+++ src/java/pt/uc/dei/aor/grupod/proj5/controllers/UserController.java
@@ -2,19 +2,34 @@
 
 package pt.uc.dei.aor.grupod.proj5.controllers;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.component.UIForm;
 import javax.inject.Named;
 import pt.uc.dei.aor.grupod.proj5.entities.Administrator;
 import pt.uc.dei.aor.grupod.proj5.entities.Student;
 import pt.uc.dei.aor.grupod.proj5.entities.User;
 
 @Named
+@RequestScoped
 public class UserController {
     
     private User user;
     private Student student;
     private Administrator admin;
-    private String email;
-    private String password;
+    private String studentEmail;
+    private String studentPassword;
+    private String adminEmail;
+    private String adminPassword;
+    private String confirmPassword;
+    private UIForm login;
+    private UIForm newRegistration;
+    private UIForm adminLogin;
+    
+    @PostConstruct
+    public void init(){
+        student = new Student();
+    }
 
     public User getUser() {
         return user;
@@ -40,26 +55,70 @@ public class UserController {
         this.admin = admin;
     }
 
-    public String getEmail() {
-        return email;
+    public String getStudentEmail() {
+        return studentEmail;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
     }
 
-    public String getPassword() {
-        return password;
+    public String getStudentPassword() {
+        return studentPassword;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setStudentPassword(String studentPassword) {
+        this.studentPassword = studentPassword;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
     }
     
+    public UIForm getLogin() {
+        return login;
+    }
+
+    public void setLogin(UIForm login) {
+        this.login = login;
+    }
+
+    public UIForm getNewRegistration() {
+        return newRegistration;
+    }
+
+    public void setNewRegistration(UIForm newRegistration) {
+        this.newRegistration = newRegistration;
+    }
+
+    public UIForm getAdminLogin() {
+        return adminLogin;
+    }
+
+    public void setAdminLogin(UIForm adminLogin) {
+        this.adminLogin = adminLogin;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
     
-    
-    
-   
     
     /**
      * Creates a new student
@@ -67,9 +126,54 @@ public class UserController {
      */
     public String newStudent(){
         
-        
+        return "";
+    }
+    
+    /**
+     * Verify the Student credentials
+     * @return The string that leads to the xhtml page 
+     */
+    public String verifyStudent(){
         
         return "";
     }
+    
+    /**
+     * Verify the Administrator credentials
+     * @return The string that leads to the xhtml page 
+     */
+    public String verifyAdmin(){
+        
+        return "";
+    }
+    
+    
+    /**
+     * Changes to the new Registration form
+     */
+    public void goToNewRegistration(){
+        newRegistration.setRendered(true);
+        login.setRendered(false);
+        adminLogin.setRendered(false);
+    }
+    
+    /**
+     * Changes to the admin login form
+     */
+    public void goToAdminLogin(){
+        newRegistration.setRendered(false);
+        login.setRendered(false);
+        adminLogin.setRendered(true);
+    }
+    
+    /**
+     * Changes to the login form
+     */
+    public void goToLogin(){
+        newRegistration.setRendered(false);
+        login.setRendered(true);
+        adminLogin.setRendered(false);
+    }
+    
     
 }

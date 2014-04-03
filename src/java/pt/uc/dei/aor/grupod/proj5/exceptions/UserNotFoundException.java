9fc44c7950e54ed3149d09f9f896d3b814a520db
Pedro Pamplona <pcrpamplona@gmail.com>
2014-04-03 12:38:40 +0100

    Merge origin/master

---
+++ src/java/pt/uc/dei/aor/grupod/proj5/exceptions/UserNotFoundException.java
@@ -0,0 +1,12 @@
+
+
+package pt.uc.dei.aor.grupod.proj5.exceptions;
+
+
+public class UserNotFoundException extends Exception{
+    
+    public UserNotFoundException(){
+        super("User not found");
+    }
+    
+}
--- src/java/pt/uc/dei/aor/grupod/proj5/facades/StudentFacade.java
+++ src/java/pt/uc/dei/aor/grupod/proj5/facades/StudentFacade.java
@@ -6,14 +6,24 @@
 package pt.uc.dei.aor.grupod.proj5.facades;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.RollbackException;
+import javax.servlet.http.HttpSession;
 import pt.uc.dei.aor.grupod.proj5.entities.Edition;
+import pt.uc.dei.aor.grupod.proj5.entities.Project;
 import pt.uc.dei.aor.grupod.proj5.entities.Student;
 import pt.uc.dei.aor.grupod.proj5.exceptions.DuplicateEmailException;
+import pt.uc.dei.aor.grupod.proj5.exceptions.PassDontMatchException;
+import pt.uc.dei.aor.grupod.proj5.exceptions.PasswordNotCorrectException;
+import pt.uc.dei.aor.grupod.proj5.exceptions.UserNotFoundException;
+import pt.uc.dei.aor.projeto3.grupod.utilities.EncriptMD5;
 
 /**
  *
@@ -24,6 +34,10 @@ public class StudentFacade extends AbstractFacade<Student> {
 
     @PersistenceContext(unitName = "AutoAvaliacaoPU")
     private EntityManager em;
+
+    private String emailExists;
+
+    private String passMissmatch;
 
     /**
      *
@@ -41,12 +55,36 @@ public class StudentFacade extends AbstractFacade<Student> {
         super(Student.class);
     }
 
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    public String getEmailExists() {
+        return emailExists;
+    }
+
+    public void setEmailExists(String emailExists) {
+        this.emailExists = emailExists;
+    }
+
+    public String getPassMissmatch() {
+        return passMissmatch;
+    }
+
+    public void setPassMissmatch(String passMissmatch) {
+        this.passMissmatch = passMissmatch;
+    }
+
     /**
      * this method finds all the students in the database, catches an Exception
      * if the transaction was unsuccessful uses the namedquery
      * Student.findAllStudents
      *
-     * @return the result list of the
+     * @return the result list
      */
     public List<Student> findAllStudents() {
         try {
@@ -83,7 +121,7 @@ public class StudentFacade extends AbstractFacade<Student> {
      * @param email
      *
      * @return the result of the query, if NoResultException is catched, the
-     * method returns null
+     * method returns the student that was found by the query
      */
     public Student findStudentsByEmail(String email) {
 
@@ -103,34 +141,187 @@ public class StudentFacade extends AbstractFacade<Student> {
     }
 
     /**
-     * t
+     * this method finds a list of students by the yearOfRegistration, use the
+     * named query Student.findStudentByYearOfRegistration
      *
      * @param yearOfRegistration
-     * @return
+     * @return the result list if the query is successful and null if the query
+     * can't find any student
      */
     public List<Student> findStudentsByYearOfRegistration(int yearOfRegistration) {
-        Query q = em.createNamedQuery("Student.findStudentByYearOfRegistration");
-        q.setParameter("yearOfRegistration", yearOfRegistration);
-        return q.getResultList();
+        try {
+            Query q = em.createNamedQuery("Student.findStudentByYearOfRegistration");
+            q.setParameter("yearOfRegistration", yearOfRegistration);
+            return q.getResultList();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     /**
+     * this method finds a list of students by the edition, uses the named query
+     * Student.findStudentByEdition
      *
      * @param edition
      * @return
      */
     public List<Student> findStudentsByEdition(Edition edition) {
-        Query q = em.createNamedQuery("Student.findStudentByEdition");
-        q.setParameter("edition", edition);
-        return q.getResultList();
+        try {
+            Query q = em.createNamedQuery("Student.findStudentByEdition");
+            q.setParameter("edition", edition);
+            return q.getResultList();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    public boolean checksEmail(String email) throws DuplicateEmailException {
+    /**
+     * checks if there is already an email registered by a student in a database
+     *
+     * @param student
+     * @throws DuplicateEmailException if there is already an email in a
+     * database
+     */
+    public void checksEmail(Student student) throws DuplicateEmailException {
 
-        Student s = findStudentsByEmail(email);
-        if (s != null) {
+        Student s = findStudentsByEmail(student.getEmail());
+        if (s != null && !s.getEmail().equals(student.getEmail())) {
             throw new DuplicateEmailException();
         }
-        return false;
+
     }
+
+    /**
+     * Checks if the user exists and if the combination between Email and
+     * Password is correct
+     *
+     * @param loginEmail
+     * @param loginPassword
+     * @return s if the login is successfull, null if unsuccessfull
+     * @throws UserNotFoundException
+     * @throws PasswordNotCorrectException
+     */
+    public Student login(String loginEmail, String loginPassword) throws UserNotFoundException, PasswordNotCorrectException {
+
+        Student s = findStudentsByEmail(loginEmail);
+
+        String pass = EncriptMD5.cryptWithMD5(loginPassword);
+
+        if (s != null && s.getPassword().equals(pass)) {
+
+            return s;
+        } else {
+            if (s == null) {
+                throw new UserNotFoundException();
+            } else if (!s.getPassword().equals(pass)) {
+                throw new PasswordNotCorrectException();
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Invalidate the session
+     *
+     * @return The String that leads to a XHTML window
+     */
+    public String logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+        session.invalidate();
+
+        return "index";
+    }
+
+    /**
+     * creates a new Student, checks if the second passord matches the first
+     * password the student registered
+     *
+     * @param student
+     * @param password2
+     * @return
+     * @throws DuplicateEmailException
+     * @throws PassDontMatchException
+     */
+    public Student newStudent(Student student, String password2) throws DuplicateEmailException, PassDontMatchException {
+
+        if (student.getPassword().equals(password2)) {
+            try {
+                String pass = EncriptMD5.cryptWithMD5(password2);
+                student.setPassword(pass);
+                checksEmail(student);
+                create(student);
+                return student;
+            } catch (DuplicateEmailException ex) {
+                Logger.getLogger(StudentFacade.class.getName()).log(Level.SEVERE, null, ex);
+                throw new DuplicateEmailException();
+            }
+
+        } else {
+            throw new PassDontMatchException();
+        }
+
+    }
+
+    /**
+     * Make the changes to the user
+     *
+     * @param logedStudent
+     * @param student
+     * @param password1
+     * @param password2
+     * @return
+     */
+    public Student updateStudent(Student logedStudent, Student student,
+            String password1, String password2) {
+
+        if (!password1.isEmpty() && password1.equals(password2)) {
+
+            String pass = EncriptMD5.cryptWithMD5(password1);
+            student.setPassword(pass);
+
+            try {
+                checksEmail(student);
+                edit(student);
+
+                return student;
+            } catch (DuplicateEmailException e) {
+
+                emailExists = e.getMessage();
+                return null;
+            }
+        } else if (password1.isEmpty() && password2.isEmpty()) {
+
+            try {
+                checksEmail(student);
+                edit(student);
+                return student;
+            } catch (DuplicateEmailException e) {
+
+                emailExists = e.getMessage();
+                return null;
+            }
+        } else {
+            passMissmatch = "The passwords doesn't match";
+            return null;
+        }
+    }
+
+    public void removeStudent(Student s) {
+        try {
+            Edition e = s.getEdition();
+            e.getStudents().remove(s);
+            em.merge(e);
+
+            for (Project p : s.getProjects()) {
+                p.getStudentsthatCantEvaluate().remove(s);
+                em.merge(e);
+            }
+
+        } catch (RollbackException e) {
+
+        }
+
+    }
+
 }
